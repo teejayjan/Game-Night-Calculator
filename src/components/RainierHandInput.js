894a6154119f7099174105513bb0b33eec858873
Timothy Jan <tj.jan@outlook.com>
2021-12-28 13:56:50 -0800

    coding style update

--- src/components/RainierHandInput.js
+++ src/components/RainierHandInput.js
@@ -19,21 +19,21 @@ const RainierHandInput = ({ playerName, playerID, setBet, isDealer }) => {
                 {!isDealer && <h4> {playerName}'s bet{value && status ? `: $${value} ${status}` : ""} </h4>}
             </Grid>
             <Grid>
-                <TextField style={{ width: 100 }} size="small" type="number" variant="outlined" label="Bet" onChange={(e) => setValue(e.target.valueAsNumber)} />
+                <TextField style={{ width: 100 }} size="small" type="number" variant="outlined" label="Bet" value={value} onChange={(e) => setValue(e.target.valueAsNumber)} />
             </Grid>
             <Grid sx={{ my: 2 }} >
                 {isDealer ?
-                    <> <Button size="small" variant="contained" onClick={(e) => { onSubmit(e, "dealer") }}> Confirm </Button> </>
+                    <> <Button size="small" variant="contained" onClick={(e) => onSubmit(e, "dealer")}> Confirm </Button> </>
                     :
                     <>
                         <ButtonGroup>
-                            <Button size="small" variant="contained" color="success" onClick={(e) => { onSubmit(e, "win") }}> Win </Button>
+                            <Button size="small" variant="contained" color="success" onClick={(e) => onSubmit(e, "win")}> Win </Button>
                         </ButtonGroup>
                         <ButtonGroup>
-                            <Button size="small" variant="contained" color="error" onClick={(e) => { onSubmit(e, "lose") }}> Lose </Button>
+                            <Button size="small" variant="contained" color="error" onClick={(e) => onSubmit(e, "lose")}> Lose </Button>
                         </ButtonGroup>
                         <ButtonGroup>
-                            <Button size="small" variant="contained" color="warning" onClick={(e) => { onSubmit(e, "push") }}> Push </Button>
+                            <Button size="small" variant="contained" color="warning" onClick={(e) => onSubmit(e, "push")}> Push </Button>
                         </ButtonGroup> </>}
             </Grid>
         </>
